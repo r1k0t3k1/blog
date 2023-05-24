8ff9ef9502e47f2db401c93e84d14e854f0ed4b1
rikoteki <boiledshrimps0109@gmail.com>
2023-05-24 23:25:42 +0900

    add carbon-component & basic routing

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,44 @@
+<script>
+  import "carbon-components-svelte/css/g90.css";
+  import {
+    Header,
+    HeaderNav,
+    HeaderNavItem,
+    HeaderNavMenu,
+    SideNav,
+    SideNavItems,
+    SideNavMenu,
+    SideNavMenuItem,
+    SideNavLink,
+    SideNavDivider,
+    SkipToContent,
+    Content,
+    Grid,
+    Row,
+    Column,
+  } from "carbon-components-svelte";
+
+  let isSideNavOpen = false;
+
+  function toggle() {
+    isSideNavOpen = !isSideNavOpen;
+  }
+</script>
+
+<Header platformName="rikoteki blog" bind:isSideNavOpen>
+  <svelte:fragment slot="skip-to-content">
+    <SkipToContent />
+  </svelte:fragment>
+</Header>
+
+<SideNav bind:isOpen={isSideNavOpen}>
+  <SideNavItems>
+    <SideNavLink href="/" text="home" on:click={toggle}/>
+    <SideNavLink href="/about" text="about" on:click={toggle}/>
+    <SideNavLink href="/blog" text="blog" on:click={toggle}/>
+  </SideNavItems>
+</SideNav>
+
+<Content>
+  <slot></slot>
+</Content>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,11 @@
+<script>
+  import "carbon-components-svelte/css/g90.css";
+</script>
+
 <h1>
   Hello and welcome to my site!
 </h1>
 
-<a href="/">about my site</a>
+<a href="/">home</a>
+<a href="/about">about me</a>
+<a href="/blog">my blog</a>
